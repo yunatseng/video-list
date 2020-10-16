acb60faec7113bc6f41cb725755120fdf3d06c96
yunatseng <yy0124125@gmail.com>
2020-10-17 00:43:35 +0800

    ADD: 新增收藏頁面

---
+++ src/views/Collection.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="collection">
+    <a href="/collection"><img width="120" src="../assets/favorite.png"></a>
+    <AppTable :videos="videos"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AppTable from '@/components/AppTable.vue'
+
+export default {
+  name: 'collection',
+  data() {
+    return {
+      videos: JSON.parse(localStorage.getItem(`favorite-videos`)) || [],
+    };
+  },
+  created(){
+    // const items = { ...localStorage };
+    // console.log(items);
+  },
+  components: {
+    AppTable
+  },
+  // watch:{
+  //     videos: function() {
+  //       this.videos = JSON.parse(localStorage.getItem(`favorite-videos`))
+  //     }
+
+  // }
+}
+</script>
