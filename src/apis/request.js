c5e645f53c39861c7ece6e55f0e8ccf01cb9751f
yunatseng <yy0124125@gmail.com>
2020-10-17 00:57:07 +0800

    ADD: 環境變數

--- src/apis/request.js
+++ src/apis/request.js
@@ -1,6 +1,5 @@
 const API_BASE_URL = "https://www.googleapis.com/youtube/v3/videos";
-const API_KEY = process.env.API_KEY;
 
 export const request = ({ query = '' }) => (
-    fetch(`${API_BASE_URL}?${query}&key=${API_KEY}`).then((res) => res.json())
+    fetch(`${API_BASE_URL}?${query}&key=${process.env.VUE_APP_API_KEY}`).then((res) => res.json())
 );
